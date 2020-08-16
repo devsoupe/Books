a60461a59be78c7035f2347cce0ee4db20079282
perelandrax <luvyum4e@gmail.com>
2020-08-17 00:07:26 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 업데이트

---
+++ 한글서적/스프링 부트와 AWS로 혼자 구현하는 웹 서비스/source/freelec-springboot2-webservice/src/main/java/com/perelandrax/study/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package com.perelandrax.study.springboot.config.auth.dto;
+
+import com.perelandrax.study.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
